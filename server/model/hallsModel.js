5429a3e9b09960ccfa6c7c4b2e69fca1c82b35a8
Gandhamathan V <gandhasaroja@gmail.com>
2023-02-13 14:02:34 +0530

    model temp

--- server/model/hallsModel.js
+++ server/model/hallsModel.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
+// const hall
 
 const hallSchema = new mongoose.Schema({
   department: {
     type: String,
     required: [true, "you Must Provide a department"],
   },
-  noOfHalls: 
+  noOfHalls: {
+    type: Number,
+    default: 0,
+    min: [0, "no of halls must be a positive number"],
+  },
+  noOfCC: {
+    type: Number,
+    default: 0,
+    min: [0, "no of CC must be a positive number"],
+  },
+  halls: {},
 });
 
 const Halls = mongoose.model("Halls", hallSchema);
