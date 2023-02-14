f6fd8168a992a5e763e6b5368333b1f2c80d3d07
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-02-14 08:54:35 +0530

    blueprint

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar";
 import AdminLogin from "./pages/AdminLogin";
 import Login from "./pages/Login";
 import NewBooking from "./pages/NewBooking";
+import SeatAllocation from "./pages/SeatAllocation";
 
 const App = () => {
   return (
@@ -11,13 +12,21 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route
-        path="/"
+        path="/newHall"
         element={
           <Sidebar>
             <NewBooking />
           </Sidebar>
         }
       />
+      <Route
+        path="/"
+        element={
+          <Sidebar>
+            <SeatAllocation />
+          </Sidebar>
+        }
+      ></Route>
     </Routes>
   );
 };
--- client/src/pages/NewBooking.jsx
+++ client/src/pages/NewBooking.jsx
@@ -1,10 +1,161 @@
-
-import React from 'react'
+import React, { useState } from "react";
+import SelectDesks from "../components/SelectDesks";
+import ButtonPrimary from "../components/ButtonPrimary";
+import "./../styles/pages/NewBooking.scss";
+import { NotificationManager } from "react-notifications";
+import Select from "react-select";
 
 const NewBooking = () => {
-    return (
-        <div style={{ marginLeft: "25rem" }}>NewBooking</div>
-    )
-}
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [venue, setVenue] = useState("");
+  const [maxRow, setMaxRow] = useState("");
+  const [maxColumn, setMaxColumn] = useState("");
+  const [unselectedDesks, setUnSelectedDesks] = useState([]);
+  const [showDesks, setShowDesks] = useState(false);
 
-export default NewBooking
+  const generateBluePrint = () => {
+    console.log("hi");
+    setShowDesks(true);
+  };
+
+  const bookingTypes = [
+    { label: "Classroom", value: "CL" },
+    { label: "Computer Center (CC)", value: "CC" },
+  ];
+
+  const submitHandler = () => {
+    let bluePrint = {};
+
+    for (let i = 0; i < maxRow; i++) {
+      for (let j = 0; j < maxColumn; j++) {
+        if (!unselectedDesks.includes(`desk-${i}-${j}`)) {
+          bluePrint[`desk-${i}-${j}`] = true;
+        } else {
+          bluePrint[`desk-${i}-${j}`] = false;
+        }
+      }
+    }
+
+    console.log(bluePrint);
+  };
+
+  return (
+    <div id="new-booking-section">
+      <div>
+        <div className="input-field-container">
+          <label className="input-field-label">Name :</label>
+          <input
+            className="input-field"
+            placeholder="Eg. ITP-S12"
+            type={"text"}
+            name="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div className="input-field-container">
+          <label className="input-field-label">Type :</label>
+          <div className="select-field-container">
+            <Select
+              options={bookingTypes}
+              name="type"
+              onChange={(e) => {
+                setType(e.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className="input-field-container">
+          <label className="input-field-label">Venue : </label>
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Eg. IT PARK - CSE 2nd Floor - S12"
+            name="venue"
+            value={venue}
+            onChange={(e) => setVenue(e.target.value)}
+          />
+        </div>
+        <div className="input-field-container">
+          <label className="input-field-label">Max Row : </label>
+          <input
+            className="input-field number"
+            placeholder="Max row"
+            value={maxRow}
+            type={"number"}
+            onChange={(e) => {
+              setShowDesks(false);
+              if (e.target.value <= 20) setMaxRow(e.target.value);
+              else {
+                NotificationManager.warning(
+                  "Value should be less than 20",
+                  "Warning",
+                  5000
+                );
+              }
+            }}
+          />
+        </div>
+        <div className="input-field-container">
+          <label className="input-field-label">Max Column : </label>
+          <input
+            className="input-field number"
+            placeholder="Max col"
+            value={maxColumn}
+            type={"number"}
+            onChange={(e) => {
+              setShowDesks(false);
+              if (e.target.value <= 20) setMaxColumn(e.target.value);
+              else {
+                NotificationManager.warning(
+                  "Value should be less than 20",
+                  "Warning",
+                  5000
+                );
+              }
+            }}
+          />
+        </div>
+        <div className="button-container">
+          <ButtonPrimary
+            style={{ borderRadius: 2 }}
+            onClick={() => {
+              generateBluePrint();
+            }}
+          >
+            <p>Show Blueprint</p>
+          </ButtonPrimary>
+        </div>
+      </div>
+      <div className="desks">
+        {showDesks && (
+          <>
+            <p className="seats-available">
+              Capacity - {maxRow * maxColumn - unselectedDesks.length}
+            </p>
+            <SelectDesks
+              key={unselectedDesks}
+              unselectedDesks={unselectedDesks}
+              setUnSelectedDesks={setUnSelectedDesks}
+              maxColumn={maxColumn}
+              maxRow={maxRow}
+            />
+            <div className="book">
+              <ButtonPrimary
+                style={{ borderRadius: 2 }}
+                onClick={submitHandler}
+              >
+                Save Hall
+              </ButtonPrimary>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NewBooking;
